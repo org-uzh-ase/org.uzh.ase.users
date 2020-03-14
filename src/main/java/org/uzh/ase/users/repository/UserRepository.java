3c606ea1bb90d99ef5ce9fa5f6e03d893e0a3bba
ltoedt <laura.toedtli@uzh.ch>
2020-03-14 17:42:13 +0100

    removed unused import statement

--- src/main/java/org/uzh/ase/users/repository/UserRepository.java
+++ src/main/java/org/uzh/ase/users/repository/UserRepository.java
@@ -4,8 +4,6 @@ package org.uzh.ase.users.repository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.uzh.ase.users.models.User;
 
-import java.util.List;
-
 public interface UserRepository extends MongoRepository<User, String> {
 
     public User findByName(String name);

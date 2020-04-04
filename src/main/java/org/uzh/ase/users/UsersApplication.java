c69289365f8e7ca7c76cc15e736d0cd7d13623c6
ltoedt <laura.toedtli@uzh.ch>
2020-04-04 17:29:36 +0200

    added repositories and tests

--- src/main/java/org/uzh/ase/users/UsersApplication.java
+++ src/main/java/org/uzh/ase/users/UsersApplication.java
@@ -1,16 +1,19 @@
 package org.uzh.ase.users;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.uzh.ase.users.models.User;
+import org.uzh.ase.users.repository.UserRepository;
 
 @SpringBootApplication
-public class UsersApplication{
-
+public class UsersApplication implements CommandLineRunner {
 	public static void main(String[] args) {
-		SpringApplication.run(UsersApplication.class, args);
+		SpringApplication.run(UsersApplication.class);
 	}
 
 	@Bean
@@ -21,5 +24,14 @@ public class UsersApplication{
 				registry.addMapping("/example").allowedOrigins("http://localhost:4200");
 			}
 		};
+	}
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		userRepository.deleteAll();
+		userRepository.save(new User("Laura", "laura@test.com"));
 	}
 }

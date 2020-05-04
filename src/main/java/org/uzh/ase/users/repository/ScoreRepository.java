969561282f9cdf71205d13bff7944da22b15f47b
ltoedt <laura.toedtli@uzh.ch>
2020-05-04 17:57:59 +0200

    sonar fixes

--- src/main/java/org/uzh/ase/users/repository/ScoreRepository.java
+++ src/main/java/org/uzh/ase/users/repository/ScoreRepository.java
@@ -2,7 +2,8 @@ package org.uzh.ase.users.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.uzh.ase.users.models.Score;
+import org.uzh.ase.users.models.ScoreDB;
 
 
-public interface ScoreRepository extends MongoRepository<Score, String> {
+public interface ScoreRepository extends MongoRepository<ScoreDB, String> {
 }

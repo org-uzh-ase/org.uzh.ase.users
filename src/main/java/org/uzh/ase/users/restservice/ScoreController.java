a7a78278524c2ed7eb5d1c63ff91f3d132b3e038
ltoedt <laura.toedtli@uzh.ch>
2020-05-21 18:10:43 +0200

    add more doc

--- src/main/java/org/uzh/ase/users/restservice/ScoreController.java
+++ src/main/java/org/uzh/ase/users/restservice/ScoreController.java
@@ -28,6 +28,10 @@ public class ScoreController {
     @Autowired
     ScoreRepository repository;
 
+    /**
+     * REST API getScores(). Checkout the <a href="https://app.swaggerhub.com/apis-docs/ltoedt/ASE/0.1#/User%20Microservice/get_api_scores">Swagger Documentation</a>
+     * @return List of {@link Score}
+     */
     @GetMapping(path = "/api/scores")
     public List<Score> getScores(){
             List<ScoreDB> resultDB = repository.findAll(Sort.by(Sort.Direction.DESC, "scoreNo"));
@@ -45,6 +49,10 @@ public class ScoreController {
             return result;
     }
 
+    /**
+     * REST API postScore(). Checkout the <a href="https://app.swaggerhub.com/apis-docs/ltoedt/ASE/0.1#/User%20Microservice/post_api_scores_score">Swagger Documentation</a>
+     * @return String representation of the score
+     */
     @PostMapping(path = "/api/scores/score")
     public ResponseEntity<String> postScore(@RequestBody Score score){
             repository.save(new ScoreDB(score));
